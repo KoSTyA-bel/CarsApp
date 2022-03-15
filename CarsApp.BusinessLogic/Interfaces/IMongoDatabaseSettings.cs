209168d957337d14dfbe0190fd40c48f42eceae5
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-03-15 17:55:06 +0300

    Added settings for the mongo database.

---
+++ CarsApp.BusinessLogic/Interfaces/IMongoDatabaseSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsApp.Businesslogic.Interfaces
+{
+    public interface IMongoDatabaseSettings<T> where T: class 
+    {
+        string CollectionName { get; set; }
+
+        string ConnectionString { get; set; }
+
+        string DatabaseName { get; set; }
+    }
+}
---
+++ CarsApp.BusinessLogic/Settings/EngineDatabaseSettings.cs
@@ -0,0 +1,65 @@
+﻿using CarsApp.Businesslogic.Entities;
+using CarsApp.Businesslogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsApp.Businesslogic.Settings;
+
+public class EngineDatabaseSettings : IMongoDatabaseSettings<Engine>
+{
+    private string _collectionName;
+    private string _connectionString;
+    private string _databaseName;
+
+    public EngineDatabaseSettings(string collectionName, string connectionString, string databaseName)
+    {
+        _collectionName = collectionName ?? throw new ArgumentNullException(nameof(collectionName));
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        _databaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+    }
+
+    public string CollectionName
+    {
+        get => _collectionName;
+        set
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _collectionName = value;
+        }
+    }
+
+    public string ConnectionString
+    {
+        get => _connectionString;
+        set
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _connectionString = value;
+        }
+    }
+
+    public string DatabaseName
+    {
+        get => _databaseName;
+        set
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _databaseName = value;
+        }
+    }
+}
