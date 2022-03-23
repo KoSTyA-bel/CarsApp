93b0b6f01bf1df380ea224e6699146ef579c83d0
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-03-23 18:31:07 +0300

    Added description of entities.

--- CarsApp.BusinessLogic/Entities/Car.cs
+++ CarsApp.BusinessLogic/Entities/Car.cs
@@ -1,17 +1,42 @@
 ﻿namespace CarsApp.Businesslogic.Entities;
 
+/// <summary>
+/// Class container for storing information about car.
+/// </summary>
 public class Car : IEquatable<Car>
 {
+    /// <summary>
+    /// Car id.
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// Car name.
+    /// </summary>
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Engine id.
+    /// </summary>
     public int? EngineId { get; set; }
 
+    /// <summary>
+    /// Car engine.
+    /// </summary>
     public Engine? Engine { get; set; }
 
+    /// <summary>
+    /// Check if the passed object is equal to an instance.
+    /// </summary>
+    /// <param name="obj">Object.</param>
+    /// <returns>True, if the objects are equal. False in other cases.</returns>
     public override bool Equals(object? obj) => Equals(obj as Car);
 
+    /// <summary>
+    /// Check if the passed object is equal to an instance.
+    /// </summary>
+    /// <param name="obj">Another car.</param>
+    /// <returns>True, if the objects are equal. False in other cases.</returns>
     public bool Equals(Car? other)
     {
         if (other is null)
--- CarsApp.BusinessLogic/Entities/Engine.cs
+++ CarsApp.BusinessLogic/Entities/Engine.cs
@@ -1,13 +1,28 @@
 ﻿namespace CarsApp.Businesslogic.Entities;
 
+/// <summary>
+/// Class container for storing information about engine.
+/// </summary>
 public class Engine : IEquatable<Engine>
 {
+    /// <summary>
+    /// Engine id.
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// Engine name.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// List of the cars with this engine.
+    /// </summary>
     public List<Car> Cars { get; set; }
 
+    /// <summary>
+    /// Creates a new instanse of <see cref="Engine"/>.
+    /// </summary>
     public Engine()
     {
         Name = String.Empty;
@@ -18,8 +33,18 @@ public class Engine : IEquatable<Engine>
 
     public static bool operator !=(Engine left, Engine right) => left is null ? false : !left.Equals(right);
 
+    /// <summary>
+    /// Check if the passed object is equal to an instance.
+    /// </summary>
+    /// <param name="obj">Object.</param>
+    /// <returns>True, if the objects are equal. False in other cases.</returns>
     public override bool Equals(object? obj) => Equals(obj as Engine);
 
+    /// <summary>
+    /// Check if the passed object is equal to an instance.
+    /// </summary>
+    /// <param name="obj">Another engine.</param>
+    /// <returns>True, if the objects are equal. False in other cases.</returns>
     public bool Equals(Engine? other)
     {
         if (other is null)
@@ -35,6 +60,7 @@ public class Engine : IEquatable<Engine>
         return Id.Equals(other.Id) && string.Equals(Name, other.Name) && Cars.Equals(other.Cars);
     }
 
+    /// <inheritdoc/>
     public override int GetHashCode()
     {
         int res = Id.GetHashCode();
